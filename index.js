50cecd21b7a49df1a9e7b7ce356012c0533cb7e9
Simon Cordova <>
2020-11-22 15:57:15 -0500

    new changes

--- index.js
+++ index.js
@@ -138,54 +138,54 @@ document.addEventListener("DOMContentLoaded", function () {
       { id: "onion", title: "onion" },
       { id: "potato", title: "potato" },
     ],
-    // numbers: [
-    //   {
-    //     id: 0,
-    //     title: "zero",
-    //   },
-    //   {
-    //     id: 1,
-    //     title: "one",
-    //   },
-    //   {
-    //     id: 2,
-    //     title: "two",
-    //   },
-    //   {
-    //     id: 3,
-    //     title: "three",
-    //   },
-    //   {
-    //     id: 4,
-    //     title: "four",
-    //   },
-    //   {
-    //     id: 5,
-    //     title: "five",
-    //   },
-    //   {
-    //     id: 6,
-    //     title: "six",
-    //   },
-    //   {
-    //     id: 7,
-    //     title: "seven",
-    //   },
-    //   {
-    //     id: 8,
-    //     title: "eight",
-    //   },
-    //   {
-    //     id: 9,
-    //     title: "nine",
-    //   },
-    // ],
-    // shapes: [
-    //   { id: "circle", title: "circle" },
-    //   { id: "triangle", title: "triangle" },
-    //   { id: "square", title: "square" },
-    //   { id: "star", title: "star" },
-    // ],
+    numbers: [
+      {
+        id: 0,
+        title: "zero",
+      },
+      {
+        id: 1,
+        title: "one",
+      },
+      {
+        id: 2,
+        title: "two",
+      },
+      {
+        id: 3,
+        title: "three",
+      },
+      {
+        id: 4,
+        title: "four",
+      },
+      {
+        id: 5,
+        title: "five",
+      },
+      {
+        id: 6,
+        title: "six",
+      },
+      {
+        id: 7,
+        title: "seven",
+      },
+      {
+        id: 8,
+        title: "eight",
+      },
+      {
+        id: 9,
+        title: "nine",
+      },
+    ],
+    shapes: [
+      { id: "circle", title: "circle" },
+      { id: "triangle", title: "triangle" },
+      { id: "square", title: "square" },
+      { id: "star", title: "star" },
+    ],
     flags: [
       {
         id: "canada",
