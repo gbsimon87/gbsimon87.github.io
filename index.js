e76a5305042083ccc21808e856cb7333ec856a00
Simon Cordova <>
2020-11-21 20:16:49 -0500

    fix img URL

--- index.js
+++ index.js
@@ -284,7 +284,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("game is:", game.querySelector("span").innerText);
         const fileName = game.querySelector("span").innerText;
         game.style.backgroundImage =
-          "url(/images/catwegoryBGImages/" + fileName + ".png)";
+          "url(/images/categoryBGImages/" + fileName + ".png)";
 
         // game.style.backgroundColor = getRandomColor();
 
